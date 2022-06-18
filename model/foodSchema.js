dce8b5f6dccb90e550563a261e74fe339dbc3c1e
prithvirajktagadinamani@gmail.com <prithvirajktagadinamani@gmail.com>
2022-06-18 12:34:25 +0530

    Gadbad hogaya

--- model/foodSchema.js
+++ model/foodSchema.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const foodSchema = mongoose.Schema({
-  _id:String,
-  name: String,
-  description: String,
-  hotelName: String,
   price: String,
   category: String,
   vegNonVeg: String,
-  time:Date
+  time: Date,
+  price: String,
+  category: String,
+  vegNonVeg: String,
+  time: Date,
 });
 
 const Food = mongoose.model("Food", foodSchema);
